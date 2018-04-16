e361913e272d9d1a59dcbcc22efdbc4b84867770
Salas <Salas@Velok>
2018-04-16 05:17:27 +0000

    Added validation patterns.

---
+++ project-user/src/main/java/edu/unk/cs406/user/validation/ValidationPatterns.java
@@ -0,0 +1,14 @@
+package edu.unk.cs406.user.validation;
+
+/**
+ * Patterns used for validation in UserEntity.
+ * @author Salas
+ *
+ */
+public class ValidationPatterns {
+	final public static String NUMBER_PATTERN = "^.*[0-9].*$";
+	final public static String LOWERCASE_PATTERN = "^.*[a-z].*$";
+	final public static String UPPERCASE_PATTERN = "^.*[A-Z].*$";
+	final public static String SYMBOL_PATTERN = "^.*[,~,!,@,#,$,%,^,&,*,(,),-,_,=,+,[,{,],},|,;,:,<,>,/,?].*$";
+	final public static String EMAIL_PATTERN = "^\\w+@[a-zA-Z_]+?\\.[a-zA-Z]{2,3}$";
+}
